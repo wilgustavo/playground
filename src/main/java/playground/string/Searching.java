345b23df335406877c53b48fa30f47541b85ce7b
wilgustavo <wilgustavo@gmail.com>
2019-06-30 16:55:57 -0300

    Boyer-Moore Search

--- src/main/java/playground/string/Searching.java
+++ src/main/java/playground/string/Searching.java
@@ -27,4 +27,42 @@ public class Searching {
         return -1;
     }
 
+    /**
+     * Búsqueda por el método Boyer-Moore.
+     *
+     * @param text Cadena de texto
+     * @param pattern Porción de texto a buscar.
+     * @return posición la primer coincidencia. Retorna -1 si no encuentra.
+     */
+    public static int boyerMooreSearch(String text, String pattern) {
+        int textLen = text.length();
+        int patternLen = pattern.length();
+        int pos = 0;
+        int cur = pos + patternLen - 1;
+        boolean igual = false;
+        while(cur >= pos) {
+            igual = pattern.charAt(cur - pos) == text.charAt(cur);
+            if(igual) {
+                cur -= 1;
+            } else {
+                char c = text.charAt(cur);
+                while(cur >= pos && pattern.charAt(cur - pos) != c) {
+                    cur -= 1;
+                }
+                if (cur < pos) {
+                    pos += patternLen;
+                } else {
+                    pos += pos + patternLen - cur -1;
+                }
+                if (pos <= textLen - patternLen) {
+                    cur = pos + patternLen - 1;
+                }
+            }
+        }
+        if (igual) {
+            return pos;
+        }
+        return -1;
+    }
+
 }
--- src/test/java/playground/string/SearchingTest.java
+++ src/test/java/playground/string/SearchingTest.java
@@ -18,4 +18,13 @@ public class SearchingTest {
         assertThat(Searching.linealSearch("abcdeef", "deef"), is(3));
         assertThat(Searching.linealSearch("abeee", "eeee"), is(-1));
     }
+
+    @Test
+    public void boyerMooreSearch() {
+        assertThat(Searching.boyerMooreSearch("", ""), is(-1));
+        assertThat(Searching.boyerMooreSearch("a", "a"), is(0));
+        assertThat(Searching.boyerMooreSearch("abcde", "bc"), is(1));
+        assertThat(Searching.boyerMooreSearch("abcdeef", "deef"), is(3));
+        assertThat(Searching.boyerMooreSearch("abeee", "eeee"), is(-1));
+    }
 }
